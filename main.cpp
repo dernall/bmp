c5e6aeb1a4585e1eb5a3f74e19dbb675c8f4b7ca
Nikita <blacknikboard@gmail.com>
2020-05-30 02:13:14 -0700

    Fixed 24/32 diff

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 
 int main(int argc, char **argv)
 {
-    Image test("input1.bmp");
-    // Image test('b', 32, 600, 400);
-    // Image img2;
-    // img2 = test;
-    test.writeImage("result1.bmp");
-    return 0;
+	Image test("input1.bmp");
+	// Image test('b', 32, 600, 400);
+	// Image img2;
+	// img2 = test;
+	test.writeImage("result1.bmp");
+	return 0;
 } // namespace App
